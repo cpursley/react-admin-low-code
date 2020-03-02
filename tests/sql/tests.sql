97c3a88884bf28aec604858fffad7cd6d550016d
dvasdekis <dimitri@vasdekis.com.au>
2020-03-02 12:45:07 +1100

    Added permissions from Hasura users/todos tutorial

--- tests/sql/tests.sql
+++ tests/sql/tests.sql
@@ -5,13 +5,21 @@
 BEGIN;
 
 -- How many tests are we running?
-SELECT plan( 3 );
+SELECT plan( 6 );
 
 -- Define the tests
 SELECT has_table( 'todos', 'todos table is present');
 SELECT has_table( 'users', 'users table is present');
 SELECT has_extension( 'pg_catalog', 'plpgsql', 'Should have pgsql extension' );
-
+SELECT users_are(ARRAY[ 'postgres', 'hasurauser' ], 'Should have hasurauser as a user');
+SELECT table_privs_are(
+    'public', 'todos', 'hasurauser', ARRAY['SELECT', 'INSERT'],
+    'hasurauser should be able to select and insert on todos'
+);
+SELECT table_privs_are(
+    'public', 'users', 'hasurauser', ARRAY['SELECT', 'INSERT'],
+    'hasurauser should be able to select and insert on users'
+);
 -- Run the tests
 SELECT * FROM finish();
 
