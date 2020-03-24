5e1d1d64f1f4ae00ef6990a31adb9fdd38e15d3d
dvasdekis <dimitri@vasdekis.com.au>
2020-03-24 18:24:41 +1100

    Also broken

--- src/App.js
+++ src/App.js
@@ -1,33 +1,62 @@
 import React from 'react';
-
-import { Admin, Resource } from 'react-admin';
+import { fetchUtils, Admin, Resource } from 'react-admin';
 import { TodoList, TodoEdit, TodoCreate } from './todos';
 import { UserList, UserShow } from './users';
-import buildHasuraProvider from 'ra-data-hasura-graphql';
-import PostIcon from '@material-ui/icons/Book';
-import UserIcon from '@material-ui/icons/Group';
+import hasuraDataProvider from 'ra-data-hasura';
+//import PostIcon from '@material-ui/icons/Book';
+//import UserIcon from '@material-ui/icons/Group';
+import {FirebaseAuthProvider} from 'react-admin-firebase';
 
-const uri = "http://localhost:8081/v1/graphql";
+// Define Firebase auth provider
+const firebaseConfig = {
+    apiKey: "AIzaSyDL9pXM6XuKDKs-jcCuZuc263t6yAHk7xc",
+    authDomain: "react-admin-low-code.firebaseapp.com",
+    databaseURL: "https://react-admin-low-code.firebaseio.com",
+    projectId: "react-admin-low-code",
+    storageBucket: "react-admin-low-code.appspot.com",
+    messagingSenderId: "477028362078",
+    appId: "1:477028362078:web:9ab7803d11783132f6d1cf"
+  };
+const firebaseOptions = {
+  // Enable logging of react-admin-firebase
+  logging: true,
+  // Authentication persistence, defaults to 'session', options are 'session' | 'local' | 'none'
+  persistence: 'local'
+};
+
+const uri = "http://localhost:8081";
+const authProvider = FirebaseAuthProvider(firebaseConfig, firebaseOptions);
+
+// See example for dynamic headers here: https://stackoverflow.com/questions/57454456/how-to-update-data-provider-headers-dynamically-in-hasura-react-admin
 
 const App = async () => {
-    const hasuraDataProvider = await buildHasuraProvider({ clientOptions: { uri: uri }});
+    const httpClient = (url, options = {}) => {
+      if (!options.headers) {
+          options.headers = new Headers({ Accept: 'application/json' });
+      }
+      // add your own headers here
+      authProvider.getJWTToken().then(function (JWT) {
+          options.headers.set('Authorization', `Bearer ${JWT}`)});
+      return fetchUtils.fetchJson(url, options);
+    };
+    const dataProvider = hasuraDataProvider(uri, httpClient);
 
     return () => (
         <Admin
-            dataProvider={hasuraDataProvider}
-            // authProvider={authProvider}
+            dataProvider={dataProvider}
+            authProvider={authProvider}
             // dashboard={Dashboard}
         >
             <Resource
                 name="todos"
-                icon={PostIcon}
+                //icon={PostIcon}
                 list={TodoList}
                 edit={TodoEdit}
                 create={TodoCreate}
             />
             <Resource
                 name="users"
-                icon={UserIcon}
+                //icon={UserIcon}
                 list={UserList}
                 show={UserShow}
             />
