f6fbe0ec85768a4ac4c5a4c79e2796f4eba5de0c
dvasdekis <dimitri@vasdekis.com.au>
2020-03-24 18:34:57 +1100

    Close to working!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import {Admin, Resource} from 'react-admin';
+import {fetchUtils, Admin, Resource} from 'react-admin';
 import {TodoCreate, TodoEdit, TodoList} from './todos';
 import {UserList, UserShow} from './users';
-import buildHasuraProvider from 'ra-data-hasura-graphql';
-import PostIcon from '@material-ui/icons/Book';
-import UserIcon from '@material-ui/icons/Group';
-import {ApolloClient} from 'apollo-client';
-import {createHttpLink} from 'apollo-link-http';
-import {setContext} from 'apollo-link-context';
-import {InMemoryCache} from 'apollo-cache-inmemory';
+import hasuraDataProvider from 'ra-data-hasura';
+//import PostIcon from '@material-ui/icons/Book';
+//import UserIcon from '@material-ui/icons/Group';
 import {FirebaseAuthProvider} from 'react-admin-firebase';
 
 
@@ -31,56 +27,41 @@ const firebaseOptions = {
 
 
 // Define Hasura data provider
-const hasuraUri = "http://localhost:8081/v1/graphql";
+const hasuraUri = "http://localhost:8081";
 
 
 // Define main App
 const App = async () => {
     const authProvider = FirebaseAuthProvider(firebaseConfig, firebaseOptions);
     //let firebaseJWT = await authProvider.getJWTToken();
-    //console.log(`JWT: ${firebaseJWT}`);
-    // When we aren't logged in, we want to use the anonymous hasura role. When we are, change the client to include the bearer token.
-    // Below sets headers to anonymous if JWT is null, otherwise it sets them properly
-    // var apolloHeaders = (!firebaseJWT) ? {'x-hasura-role': 'anonymous'} : {authorization: `Bearer ${firebaseJWT}`, };
-    // var usertype = (!firebaseJWT) ? 'null condition' : 'logged in';
-
-    let hasuraDataProvider = authProvider.getJWTToken().then(function (JWT) {
-        const httpLink = createHttpLink({
-            uri: hasuraUri,
-        });
-        const authLink = setContext((_, { headers }) => {
-          console.log(`Token: ${JWT}`);
-          // return the headers to the context so httpLink can read them
-          return {
-            headers: {
-              ...headers,
-              ...(JWT ? {authorization: `Bearer ${JWT}`} : {}),
-            }
-          }
-        });
-        const apolloClientInstance = new ApolloClient({
-          link: authLink.concat(httpLink),
-          cache: new InMemoryCache(),
-        });
-        return buildHasuraProvider({client: apolloClientInstance})
+    const httpClient = (url, options = {}) => {
+      if (!options.headers) {
+          options.headers = new Headers({ Accept: 'application/json' });
+      }
+      // add your own headers here
+      authProvider.getJWTToken().then(function (JWT) {
+          options.headers.set('Authorization', `Bearer ${JWT}`)});
+      return fetchUtils.fetchJson(url, options);
     };
+    const dataProvider = hasuraDataProvider(hasuraUri, httpClient);
+
 
     return () => (
         <Admin
-            dataProvider={hasuraDataProvider}
+            dataProvider={dataProvider}
             authProvider={authProvider}
             // dashboard={Dashboard}
         >
             <Resource
                 name="todos"
-                icon={PostIcon}
+                //icon={PostIcon}
                 list={TodoList}
                 edit={TodoEdit}
                 create={TodoCreate}
             />
             <Resource
                 name="users"
-                icon={UserIcon}
+                //icon={UserIcon}
                 list={UserList}
                 show={UserShow}
             />
