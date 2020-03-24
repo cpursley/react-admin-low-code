9145f7f62017e00bdea24198863fae71a5e1b42f
dvasdekis <dimitri@vasdekis.com.au>
2020-03-24 18:02:07 +1100

    Now getting Malformed Headers problem, like https://stackoverflow.com/questions/57454456/how-to-update-data-provider-headers-dynamically-in-hasura-react-admin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Admin, Resource } from 'react-admin';
-import { TodoList, TodoEdit, TodoCreate } from './todos';
-import { UserList, UserShow } from './users';
+import {Admin, Resource} from 'react-admin';
+import {TodoCreate, TodoEdit, TodoList} from './todos';
+import {UserList, UserShow} from './users';
 import buildHasuraProvider from 'ra-data-hasura-graphql';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
-import ApolloClient from 'apollo-boost';
-import { FirebaseAuthProvider } from 'react-admin-firebase';
+import {ApolloClient} from 'apollo-client';
+import {createHttpLink} from 'apollo-link-http';
+import {setContext} from 'apollo-link-context';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {FirebaseAuthProvider} from 'react-admin-firebase';
 
 
 // Define Firebase auth provider
@@ -25,7 +28,7 @@ const firebaseOptions = {
   // Authentication persistence, defaults to 'session', options are 'session' | 'local' | 'none'
   persistence: 'local'
 };
-const authProvider = FirebaseAuthProvider(firebaseConfig, firebaseOptions);
+
 
 // Define Hasura data provider
 const hasuraUri = "http://localhost:8081/v1/graphql";
@@ -33,13 +36,34 @@ const hasuraUri = "http://localhost:8081/v1/graphql";
 
 // Define main App
 const App = async () => {
-    let firebaseJWT = await authProvider.getJWTToken();
-    console.log(`JWT: ${firebaseJWT}`);
+    const authProvider = FirebaseAuthProvider(firebaseConfig, firebaseOptions);
+    //let firebaseJWT = await authProvider.getJWTToken();
+    //console.log(`JWT: ${firebaseJWT}`);
     // When we aren't logged in, we want to use the anonymous hasura role. When we are, change the client to include the bearer token.
     // Below sets headers to anonymous if JWT is null, otherwise it sets them properly
-    var apolloHeaders = (!firebaseJWT) ? {'x-hasura-role': 'anonymous'} : {authorization: `Bearer ${firebaseJWT}`, 'x-hasura-role': 'user'};
-    let apolloClientInstance = new ApolloClient({uri: hasuraUri, headers: apolloHeaders});
-    let hasuraDataProvider = await buildHasuraProvider({ client: apolloClientInstance });
+    // var apolloHeaders = (!firebaseJWT) ? {'x-hasura-role': 'anonymous'} : {authorization: `Bearer ${firebaseJWT}`, };
+    // var usertype = (!firebaseJWT) ? 'null condition' : 'logged in';
+
+    let hasuraDataProvider = authProvider.getJWTToken().then(function (JWT) {
+        const httpLink = createHttpLink({
+            uri: hasuraUri,
+        });
+        const authLink = setContext((_, { headers }) => {
+          console.log(`Token: ${JWT}`);
+          // return the headers to the context so httpLink can read them
+          return {
+            headers: {
+              ...headers,
+              ...(JWT ? {authorization: `Bearer ${JWT}`} : {}),
+            }
+          }
+        });
+        const apolloClientInstance = new ApolloClient({
+          link: authLink.concat(httpLink),
+          cache: new InMemoryCache(),
+        });
+        return buildHasuraProvider({client: apolloClientInstance})
+    };
 
     return () => (
         <Admin
