938194f0a1855fa758a38d13d9cf62f12b20a84b
dvasdekis <dimitri@vasdekis.com.au>
2020-03-26 10:36:14 +1100

    This displays HandleAuthLogin: invalid credentials

--- src/App.js
+++ src/App.js
@@ -1,67 +1,56 @@
-import React from 'react';
-import { fetchUtils, Admin, Resource } from 'react-admin';
-import { TodoList, TodoEdit, TodoCreate } from './todos';
-import { UserList, UserShow } from './users';
-import hasuraDataProvider from 'ra-data-hasura';
+import React from "react";
+import { fetchUtils, Admin, Resource } from "react-admin";
+import { TodoCreate, TodoEdit, TodoList } from "./todos";
+import { UserList, UserShow } from "./users";
+import hasuraDataProvider from "ra-data-hasura";
 //import PostIcon from '@material-ui/icons/Book';
 //import UserIcon from '@material-ui/icons/Group';
-import {FirebaseAuthProvider} from 'react-admin-firebase';
+import { FirebaseAuthProvider } from "react-admin-firebase";
 
 // Define Firebase auth provider
 const firebaseConfig = {
-    apiKey: "AIzaSyDL9pXM6XuKDKs-jcCuZuc263t6yAHk7xc",
-    authDomain: "react-admin-low-code.firebaseapp.com",
-    databaseURL: "https://react-admin-low-code.firebaseio.com",
-    projectId: "react-admin-low-code",
-    storageBucket: "react-admin-low-code.appspot.com",
-    messagingSenderId: "477028362078",
-    appId: "1:477028362078:web:9ab7803d11783132f6d1cf"
-  };
+  apiKey: "AIzaSyDL9pXM6XuKDKs-jcCuZuc263t6yAHk7xc",
+  authDomain: "react-admin-low-code.firebaseapp.com",
+  databaseURL: "https://react-admin-low-code.firebaseio.com",
+  projectId: "react-admin-low-code",
+  storageBucket: "react-admin-low-code.appspot.com",
+  messagingSenderId: "477028362078",
+  appId: "1:477028362078:web:9ab7803d11783132f6d1cf"
+};
 const firebaseOptions = {
   // Enable logging of react-admin-firebase
   logging: true,
   // Authentication persistence, defaults to 'session', options are 'session' | 'local' | 'none'
-  persistence: 'local'
+  persistence: "local"
 };
-
-const uri = "http://localhost:8081";
 const authProvider = FirebaseAuthProvider(firebaseConfig, firebaseOptions);
 
-// See example for dynamic headers here: https://stackoverflow.com/questions/57454456/how-to-update-data-provider-headers-dynamically-in-hasura-react-admin
+// Define Hasura data provider
+const hasuraUri = "http://localhost:8081";
+const JWT = authProvider.getJWTToken().then(function (JWT) {return JWT});
+const httpClient = (url, options = {}) => {
+      options.headers = new Headers({ Accept: 'application/json' });
+      options.headers.set('Authorization', `Bearer ${JWT}`);
+  return fetchUtils.fetchJson(url, options)
+};
+const dataProvider = hasuraDataProvider(hasuraUri, httpClient);
 
-const App = async () => {
-    const httpClient = (url, options = {}) => {
-      if (!options.headers) {
-          options.headers = new Headers({ Accept: 'application/json' });
-      }
-      // add your own headers here
-      authProvider.getJWTToken().then(function (JWT) {
-          options.headers.set('Authorization', `Bearer ${JWT}`)});
-      return fetchUtils.fetchJson(url, options);
-    };
-    const dataProvider = hasuraDataProvider(uri, httpClient);
-
-    return () => (
-        <Admin
-            dataProvider={dataProvider}
-            authProvider={authProvider}
-            // dashboard={Dashboard}
-        >
-            <Resource
-                name="todos"
-                //icon={PostIcon}
-                list={TodoList}
-                edit={TodoEdit}
-                create={TodoCreate}
-            />
-            <Resource
-                name="users"
-                //icon={UserIcon}
-                list={UserList}
-                show={UserShow}
-            />
-        </Admin>
-    );
+// Define main App
+const App = () => {
+  return (
+    <Admin
+      dataProvider={dataProvider}
+      authProvider={authProvider}
+    >
+      <Resource
+        name="todos"
+        list={TodoList}
+        edit={TodoEdit}
+        create={TodoCreate}
+      />
+      <Resource name="users" list={UserList} show={UserShow} />
+    </Admin>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import getApp from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-getApp().then( App =>
-               ReactDOM.render(<App />, document.getElementById('root'))
-             );
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
