0b7bafd7a5215aaf76ba49f3e148179c3efa2246
Jasper Bernales <bernalesjasper@gmail.com>
2020-06-10 17:28:49 +0800

    added types to graphql adapter

--- src/get-app.tsx
+++ src/get-app.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import buildHasuraProvider from 'ra-data-hasura-graphql';
 import App from './App'
+import buildHasuraProvider from 'ra-data-hasura-graphql';
 
 const uri = "https://low-code-api.herokuapp.com/v1/graphql";
 
 export default async function getApp() {
-  const provider: Function = await buildHasuraProvider({ clientOptions: { uri: uri }})
+  const provider = await buildHasuraProvider({ clientOptions: { uri: uri }})
   return () => (
     <App dataProvider={provider} />
   )
