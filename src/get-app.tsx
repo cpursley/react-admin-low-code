6a2ad43ca0af28b95995db51f5d0eb00ce3c6311
Chase Pursley <chasepursley@gmail.com>
2021-11-01 12:45:36 -0400

    Use hasura cloud and swap uri

--- src/get-app.tsx
+++ src/get-app.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import App from './App'
 import buildHasuraProvider from 'ra-data-hasura-graphql';
 
-const uri = "https://low-code-api.herokuapp.com/v1/graphql";
+const uri = "https://react-admin-low-code.hasura.app/v1/graphql";
 
 export default async function getApp() {
   const provider = await buildHasuraProvider({ clientOptions: { uri: uri }})
